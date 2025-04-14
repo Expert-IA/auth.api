77b266b58651b16347a2a0ae57ff4261003bffbc
Samuel <samuks2005@outlook.com>
2025-04-14 19:54:21 -0300

    first commit

---
+++ src/test/java/com/adv_investor/auth_module/AuthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adv_investor.auth_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
