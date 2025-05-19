cdc19b41b33a8bfdf61aabc0636c4a6b896b0548
Samuel <samuks2005@outlook.com>
2025-05-19 20:18:02 -0300

    Refactor authentication and health check controllers; add AuthenticationDTO

---
+++ src/main/java/com/adv_investor/auth_module/controllers/HeathCheckController.java
@@ -0,0 +1,14 @@
+package com.adv_investor.auth_module.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
+public class HeathCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
